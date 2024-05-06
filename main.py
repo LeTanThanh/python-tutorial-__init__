8656729f7e061070539bbe914718e0da4765866a
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-07 00:37:04 +0700

    Introduction to the Python __init__() method

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+from person import Person
+
 if __name__ == "__main__":
-  print("Python __init__")
+  # Introduction to the Python __init__() method
+
+  person = Person("John", 25)
+  print(f"I'm {person.name}. I'm {person.age} years old.")
