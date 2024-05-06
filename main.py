6c2ba83c78ae231c90e6aa63de7f3a9e92d1f52e
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-07 00:38:14 +0700

    The __init__ method with default parameters

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ from person import Person
 if __name__ == "__main__":
   # Introduction to the Python __init__() method
 
-  person = Person("John", 25)
+  # person = Person("John", 25)
+  # print(f"I'm {person.name}. I'm {person.age} years old.")
+
+  # The __init__ method with default parameters
+
+  person = Person("John")
   print(f"I'm {person.name}. I'm {person.age} years old.")
--- person.py
+++ person.py
@@ -1,4 +1,4 @@
 class Person:
-  def __init__(self, name, age):
+  def __init__(self, name, age = 22):
     self.name = name
     self.age = age
